4417acded4250efe2341648e229762069d851d8f
Qwasyx <qwasyx3000@gmail.com>
2024-03-26 21:37:07 +0100

    slight DOM cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
   );
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4">
-      <div className="flex">{content}</div>
+    <section className="flex flex-col items-center justify-center">
+      {content}
     </section>
   );
 }
